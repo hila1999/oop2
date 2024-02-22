3e01221643b6983e70b4fe9df2e795b72798dbde
hila1999 <118314052+hila1999@users.noreply.github.com>
2024-02-22 16:42:36 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,75 +1,62 @@
-from Post import PostFactory
+from typing import Set
+
+from User import User
 
 
-class User:
+class SocialNetwork:
+    _instance = None
 
-    def __init__(self, name: str, passord: str ,is_connected: bool=None ):
+    def __new__(cls, *args, **kwargs):  #A template singclton that makes sure we only created one network
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self, name):
+        if self._initialized:
+            return
         self.name = name
-        self.passord = passord
-        self.is_connected = is_connected
-        self.followers = [] # how is follow after hime
-        self.my_notifications = []
-        self.observers = []
-        self.my_posts = []
+        self.network = {}
+        self.logged_in_users: Set[User] = set()
+        print(f"The social network {self.name} was created!")
+        self._initialized = True
 
     def __str__(self):
-        return f"User name: {self.name}, Number of posts: {self.my_posts.__len__()}, Number of followers: {self.followers.__len__()}"
+        users_info = [str(self.network[username]) for username in self.network]  # List comprehension to get user info
+        return f"{self.name} social network:\n" + "\n".join(users_info) + "\n"
 
 
 
-    def notify_observers(self, notifications):
-        for observer in self.observers:
-            observer.update(self, notifications)
 
-    def update(self, observable, notifications):
-        self.my_notifications.append(notifications)
-
-    # def like(self,post):
-    #     if not self.is_connected:
-    #         print("Not connected")
-    #         return
-    #     if self not in post.liked_by:
-    #      post.liked_by.append(self)
-    #     else:
-    #         pass
-    # def comment(self,post):
-    #     if not self.is_connected:
-    #         print("Not connected")
-    #         return
-    #     if self not in post.comments:
-    #         post.comments.append(self)
-
-    def print_notifications(self):
-        print(f"{self.name}'s notifications:")
-        for notifications in self.my_notifications:
-            print(f"{notifications}")
-    def follow(self, u2):
-        if not self.is_connected:
-            print("Not connected")
-            return
-        if self in u2.followers:
-            pass
-        if self.name != u2.name:
-            u2.followers.append(self)
-            u2.observers.append(self)
-            print(f"{self.name} started following {u2.name}")
-    def unfollow(self, u2):
-        if not self.is_connected:
-            print("Not connected")
-        if self in u2.followers:
-            u2.followers.remove(self)
-            u2.observers.remove(self)
-            print(f"{self.name} unfollowed {u2.name}")
+    def sign_up(self,username, password):
+        if username in self.network:  #Checks that the selected username is not registered by another user
+            print("Username already exists. Please choose another one.")
             return None
-        print("the user is not foolowes so we cant unfolow")
-
-
-    def publish_post(self, type: str, dataWord: str, price = None, location:str = None, available = True):
-        if not self.is_connected:
-            print("Not connected")
+        if not 4 < len(password) < 8: #Checks that the password is the correct length
+            print("Password length should be between 4 and 8 characters.")
+            return None
+        user = User(username, password)
+        self.network[username] = user
+        self.logged_in_users.add(user)
+        user.is_connected=True
+        return user
+    def log_in(self, username, password):
+        if username in self.network:
+            self.logged_in_users.add(self.network[username])
+            self.network[username].is_connected=True
+            print(f"{username} connected")
+        #add check for passord
+        else:
+            print("Username does not exist. Please choose another one.")
             return
-        post_now =PostFactory.process_type(self, type, dataWord, price, location, available = True)
-        self.my_posts.append(post_now)
-        return post_now
+    def log_out(self, username):
+        if username not in self.network:
+            print("Username does not exist. Please choose another one.")
+            return
+        if self.network[username] not in self.logged_in_users:
+            print("Username does not connected. Please choose another one.")
+            return
 
-
+        self.logged_in_users.remove(self.network[username])
+        self.network[username].is_connected = False
+        print(f"{username} disconnected")
