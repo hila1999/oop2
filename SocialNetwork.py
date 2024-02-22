96384ea98865370d41ad430a1b034d9d3956d07d
hila1999 <118314052+hila1999@users.noreply.github.com>
2024-02-22 12:38:47 +0200

    Update SocialNetwork.py

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -6,7 +6,7 @@ from User import User
 class SocialNetwork:
     _instance = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):  #A template singclton that makes sure we only created one network
         if not cls._instance:
             cls._instance = super().__new__(cls)
             cls._instance._initialized = False
@@ -29,10 +29,10 @@ class SocialNetwork:
 
 
     def sign_up(self,username, password):
-        if username in self.network:
+        if username in self.network:  #Checks that the selected username is not registered by another user
             print("Username already exists. Please choose another one.")
             return None
-        if not 4 < len(password) < 8:
+        if not 4 < len(password) < 8: #Checks that the password is the correct length
             print("Password length should be between 4 and 8 characters.")
             return None
         user = User(username, password)
@@ -48,14 +48,16 @@ class SocialNetwork:
         #add check for passord
         else:
             print("Username does not exist. Please choose another one.")
+            return
     def log_out(self, username):
         if username not in self.network:
             print("Username does not exist. Please choose another one.")
+            return
         if self.network[username] not in self.logged_in_users:
             print("Username does not connected. Please choose another one.")
+            return
 
         self.logged_in_users.remove(self.network[username])
         self.network[username].is_connected = False
         print(f"{username} disconnected")
-
 
