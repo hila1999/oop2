39bf73d47a3438b79fc42cbd9a3fe942c0f80b6a
hila1999 <118314052+hila1999@users.noreply.github.com>
2024-03-01 15:30:28 +0200

    Update SocialNetwork.py

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -6,7 +6,7 @@ from User import User
 class SocialNetwork:
     _instance = None
 
-    def __new__(cls, *args, **kwargs):  #A template singclton that makes sure we only created one network
+    def __new__(cls, *args, **kwargs):  # A template singclton that makes sure we only created one network
         if not cls._instance:
             cls._instance = super().__new__(cls)
             cls._instance._initialized = False
@@ -25,30 +25,41 @@ class SocialNetwork:
         users_info = [str(self.network[username]) for username in self.network]  # List comprehension to get user info
         return f"{self.name} social network:\n" + "\n".join(users_info) + "\n"
 
-
-
-
-    def sign_up(self,username, password):
-        if username in self.network:  #Checks that the selected username is not registered by another user
+    def sign_up(self, username, password):
+        if username in self.network:  # Checks that the selected username is not registered by another user
             print("Username already exists. Please choose another one.")
             return None
-        if not 4 < len(password) < 8: #Checks that the password is the correct length
+        if not 4 < len(password) < 8:  # Checks that the password is the correct length
             print("Password length should be between 4 and 8 characters.")
             return None
         user = User(username, password)
         self.network[username] = user
         self.logged_in_users.add(user)
-        user.is_connected=True
+        user.is_connected = True
         return user
+
+    # def sign_up(self, username: str, password: str) -> User | None:
+    #     if username in self.network:  # Checks that the selected username is not registered by another user
+    #         print("Username already exists. Please choose another one.")
+    #         return None
+    #     if not 4 < len(password) < 8:  # Checks that the password is the correct length
+    #         print("Password length should be between 4 and 8 characters.")
+    #         return None
+    #     user = User(username, password)
+    #     return user
+
     def log_in(self, username, password):
         if username in self.network:
-            self.logged_in_users.add(self.network[username])
-            self.network[username].is_connected=True
-            print(f"{username} connected")
-        #add check for passord
+            user = self.network[username]
+            if password == user.passord:
+                if self.network[username] not in self.logged_in_users:
+                    self.logged_in_users.add(self.network[username])
+                    self.network[username].is_connected = True
+                    print(f"{username} connected")
         else:
             print("Username does not exist. Please choose another one.")
             return
+
     def log_out(self, username):
         if username not in self.network:
             print("Username does not exist. Please choose another one.")
@@ -56,8 +67,6 @@ class SocialNetwork:
         if self.network[username] not in self.logged_in_users:
             print("Username does not connected. Please choose another one.")
             return
-
         self.logged_in_users.remove(self.network[username])
         self.network[username].is_connected = False
         print(f"{username} disconnected")
-
